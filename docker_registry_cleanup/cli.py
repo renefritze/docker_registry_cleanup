9fbaf4d07d504e3ba8b452bf07d47385a6cd290b
René Fritze <rene.fritze@wwu.de>
2020-09-03 17:23:35 +0200

    demo list tags with client

--- docker_registry_cleanup/cli.py
+++ docker_registry_cleanup/cli.py
@@ -1,15 +1,32 @@
 """Console script for docker_registry_cleanup."""
+import logging
 import sys
 import click
 
+from docker_registry_cleanup.docker_registry_cleanup import get_client
 
-@click.command()
+
+@click.group()
 def main(args=None):
-    """Console script for docker_registry_cleanup."""
-    click.echo("Replace this message by putting your code into "
-               "docker_registry_cleanup.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+    pass
+
+
+@main.command()
+@click.option('--namespace', help='The repo to list tags')
+@click.option('--repo', help='The repo to list tags')
+@click.option('--user', help='The repo to list tags')
+@click.option('--pw', help='The repo to list tags')
+@click.option('--verbose', help='be verbose')
+@click.option('--registry', default='https://hub.docker.com', help='docker registry to connect to')
+def list_tags(namespace, repo, user, pw, verbose, registry):
+    basic_config_args = {}
+    if verbose:
+        basic_config_args["level"] = logging.DEBUG
+
+    logging.basicConfig(**basic_config_args)
+    client = get_client(registry, user=user, pw=pw)
+    repo = client.repository(repository=repo, namespace=namespace)
+    print(list(repo.tags())[:10])
 
 
 if __name__ == "__main__":
--- docker_registry_cleanup/docker_registry_cleanup.py
+++ docker_registry_cleanup/docker_registry_cleanup.py
@@ -1,1 +1,11 @@
 """Main module."""
+
+from docker_registry_client import DockerRegistryClient
+
+
+def get_client(registry, user, pw):
+    # auth_service_url_full = f'{registry}/v2/token'
+    cl = DockerRegistryClient(host=registry, api_version=1, username=user, password=pw,
+                              )
+    # cl._base_client.check_status()
+    return cl
